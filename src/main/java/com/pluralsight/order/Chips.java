0820cb1515790962c71c7d6937c0383ae489d251
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-11-24 10:45:13 -0500

    skeleton Bread and Chips

--- src/main/java/com/pluralsight/order/Chips.java
+++ src/main/java/com/pluralsight/order/Chips.java
@@ -1,4 +1,25 @@
 package com.pluralsight.order;
 
-public class Chips {
+import com.pluralsight.IPrice;
+import com.pluralsight.Size;
+
+public class Chips implements IPrice {
+    private String type;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public double getPrice(Size size) {
+        return 1.50;
+    }
 }
