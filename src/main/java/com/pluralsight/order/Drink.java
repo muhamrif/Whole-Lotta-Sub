2372aa965e1b5fb503e7a1905ad5dafc2d75cfd7
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-11-24 10:50:48 -0500

    skeleton Drink

--- src/main/java/com/pluralsight/order/Drink.java
+++ src/main/java/com/pluralsight/order/Drink.java
@@ -1,4 +1,42 @@
 package com.pluralsight.order;
 
-public class Drink {
+import com.pluralsight.IPrice;
+import com.pluralsight.Size;
+
+public class Drink implements IPrice {
+    public Size size;
+    public String type;
+
+    public Drink(Size size, String type) {
+        this.size = size;
+        this.type = type;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public double getPrice(Size size) {
+        if (size.equals(Size.SMALL)){
+            return 2.00;
+        }else if(size.equals(Size.MEDIUM)){
+            return 2.50;
+        }else {
+            return 3.00;
+        }
+    }
 }
