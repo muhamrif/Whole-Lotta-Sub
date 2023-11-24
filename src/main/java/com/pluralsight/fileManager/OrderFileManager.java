3b6ffe31a6d08e666c6b15a49bc8ae5b7b10d4ed
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-11-24 10:19:05 -0500

    skeleton orderFileManager

--- src/main/java/com/pluralsight/fileManager/OrderFileManager.java
+++ src/main/java/com/pluralsight/fileManager/OrderFileManager.java
@@ -1,4 +1,17 @@
 package com.pluralsight.fileManager;
 
+import com.pluralsight.order.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderFileManager {
+
+    public ArrayList<Order> getSavedOrders(){
+        return null;
+    }
+
+    public void saveOrder(Order order){
+
+    }
 }
