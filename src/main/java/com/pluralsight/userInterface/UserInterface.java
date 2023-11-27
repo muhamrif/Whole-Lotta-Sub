3d5681ef87cc5fc18853a3cc4f838976264e4980
akhilsomaiya07 <akhilsomaiya7@gmail.com>
2023-11-26 20:33:12 -0500

    SandwichMenu added

--- src/main/java/com/pluralsight/userInterface/UserInterface.java
+++ src/main/java/com/pluralsight/userInterface/UserInterface.java
@@ -80,9 +80,90 @@ public class UserInterface {
 
 
     public void addSandwichMenu(){
+        Scanner input = new Scanner(System.in);
+        int bread = -1;
+        int meat = -1;
+        int cheese = -1;
+        int otherToppings = -1;
+        int sauces = -1;
+        boolean toasted = false;
+        while (bread == -1) {
+            System.out.print("Enter type of bread (1 for white and 2 for wheat 3 for rye 4 for wrap): ");
+            try {
+                bread = Integer.parseInt(input.next());
+                if (bread != 1 && bread != 2) {
+                    System.out.println("Invalid input. Please enter 1 for white or 2 for wheat 3 for rye 4 for wrap.");
+                    bread = -1;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+        while (meat == -1) {
+            System.out.print("Enter type of meat (1 for turkey, 2 for ham, 3 for salami 4 for roast beef 5 for chicken 6 for bacon ): ");
+            try {
+                meat = Integer.parseInt(input.next());
+                if (meat < 1 || meat > 3) {
+                    System.out.println("Invalid input. Please enter a valid number.");
+                    meat = -1;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+        while (cheese == -1) {
+            System.out.print("Do you want cheese (1 for yes, 2 for no): ");
+            try {
+                cheese = Integer.parseInt(input.next());
+                if (cheese != 1 && cheese != 2) {
+                    System.out.println("Invalid input. Please enter 1 for yes or 2 for no.");
+                    cheese = -1;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+        while (otherToppings == -1) {
+            System.out.print("Do you want other toppings (1 for yes, 2 for no): ");
+            try {
+                otherToppings = Integer.parseInt(input.next());
+                if (otherToppings != 1 && otherToppings != 2) {
+                    System.out.println("Invalid input. Please enter 1 for yes or 2 for no.");
+                    otherToppings = -1;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+        while (sauces == -1) {
+            System.out.print("Select sauces (1 for mayonnaise, 2 for mustard, 3 for ketchup, 4 for ranch 5 for thousand island 6 for vinaigrette): ");
+            try {
+                sauces = Integer.parseInt(input.next());
+                if (sauces < 1 || sauces > 4) {
+                    System.out.println("Invalid input. Please enter a valid number.");
+                    sauces = -1;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+        while (toasted == false) {
+            System.out.print("Do you want your sandwich toasted (yes or no): ");
+            String answer = input.nextLine();
+            if (answer.equalsIgnoreCase("yes")) {
+                toasted = true;
+            } else if (answer.equalsIgnoreCase("no")) {
+                toasted = false;
+            } else {
+                System.out.println("Invalid input. Please enter yes or no.");
+            }
+        }
 
-
+        System.out.println("Your sandwich order is placed!");
+        input.close();
     }
+
+
 
     public void addDrinkMenu(){
         Scanner scanner = new Scanner(System.in);
