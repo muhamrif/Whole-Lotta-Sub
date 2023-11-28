da98f2584bfdc59b11407abf649bedd1395dc4a8
akhilsomaiya07 <56009275+akhilsomaiya07@users.noreply.github.com>
2023-11-28 17:38:40 -0500

    Progressbar (#35)
    * color part done
    
    * added loading bar

---
+++ src/main/java/LoadingBar/ProgressBar.java
@@ -0,0 +1,40 @@
+package LoadingBar;
+
+
+import com.pluralsight.userInterface.ConsoleColors;
+
+public class ProgressBar {
+    public static void main() {
+        //char[] animationChars = new char[] {'|', '/', '-', '\\'};
+        char[] animationChars = new char[] {'⣾', '⣽', '⣻', '⢿', '⣟', '⣯', '⣷'};
+        for (int i = 0; i <= 100; i++) {
+            System.out.print("Processing: " + i + "% " + animationChars[i % 4] + "\r");
+
+
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Processing: Done!");
+    }
+    public static void bar() {
+        int totalSteps = 100;
+        for (int i = 0; i <= totalSteps; i++) {
+            int progress = i * 100 / totalSteps;
+            String progressBar = "[" + ConsoleColors.GREEN+ConsoleColors.GREEN_BACKGROUND+"=".repeat(progress)+ConsoleColors.RESET +" ".repeat(100 - progress) +  "]";
+            System.out.print("\rWELCOME TO WHOLE-LOTTA-SUB: " + progressBar + " " + progress + "%");
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+
+
+
